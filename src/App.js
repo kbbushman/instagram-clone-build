e3aa6a1bfc1a71d02540fdeb4cc96d5f56140ed4
kbbushman <k.bushman@yahoo.com>
2021-03-02 23:56:45 -0800

    Adds UserContext from React createContext

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import CreatePost from './components/CreatePost';
 import PostList from './components/PostList';
 
+export const UserContext = React.createContext();
+
 function App() {
   const [user, setUser] = React.useState('');
   const [posts, setPosts] = React.useState([]);
@@ -21,11 +23,11 @@ function App() {
   }
 
   return (
-    <>
+    <UserContext.Provider value={user}>
       <Header user={user} setUser={setUser} />
       <CreatePost user={user} handleAddPost={handleAddPost} />
       <PostList posts={posts} />
-    </>
+    </UserContext.Provider>
   );
 }
 
