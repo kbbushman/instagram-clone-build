74356cd448df94dd7f59ffe95f6328d886c2a350
kbbushman <k.bushman@yahoo.com>
2021-03-03 23:31:36 -0800

    Adds delete post case

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,11 @@
 function postReducer(state, action) {
   switch (action.type) {
-    case "ADD_POST":
+    case 'ADD_POST':
       const newPost = action.payload.post;
       return { posts: [newPost, ...state.posts] };
+    case 'DELETE_POST':
+      const deletedPostId = action.payload.id;
+      return { posts: state.posts.filter((post) => post.id !== deletedPostId) };
     default:
       return state;
   }
