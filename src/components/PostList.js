4d1f53b17e4fbd40296bf16af8fac483639314c2
kbbushman <k.bushman@yahoo.com>
2021-03-03 20:35:39 -0800

    Replaces map index with temp post id for key value

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,7 @@
 import Post from './Post';
 
 function PostList({ posts }) {
-  return posts.map((post, index) => <Post key={index} {...post} />);
+  return posts.map((post) => <Post key={post.id} {...post} />);
 }
 
 export default PostList;
