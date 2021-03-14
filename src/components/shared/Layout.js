c91eee84c98b3cc4d7a1977efb6b2cc8d32e82ed
kbbushman <k.bushman@yahoo.com>
2021-03-14 14:25:31 -0700

    Adds base layout with nav and seo

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { useLayoutStyles } from "../../styles";
+import React from 'react';
+import { useLayoutStyles } from '../../styles';
+import SEO from './Seo';
+import Navbar from './Navbar';
 
-function Layout() {
-  useLayoutStyles();
+function Layout({ children, title, marginTop = 60 }) {
+  const classes = useLayoutStyles();
 
-  return <div>Layout</div>;
+  return (
+    <section className={classes.section}>
+      <SEO title={title} />
+      <Navbar />
+      <main className={classes.main} style={{ marginTop }}>
+        <section className={classes.childrenWrapper}>
+          <div className={classes.children}>
+            {children}
+          </div>
+        </section>
+      </main>
+    </section>
+  );
 }
 
 export default Layout;
