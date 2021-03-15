08d07a6ebc07dbcdd519769239408a4a878bfc34
kbbushman <k.bushman@yahoo.com>
2021-03-15 16:14:33 -0700

    Adds search results tooltip

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { AppBar, Avatar, Hidden, InputBase } from '@material-ui/core';
+import { AppBar, Avatar, Fade, Grid, Hidden, InputBase, Typography } from '@material-ui/core';
 import {
   LoadingIcon,
   AddIcon,
@@ -12,9 +12,9 @@ import {
   HomeActiveIcon,
 
 } from '../../icons';
-import { useNavbarStyles } from '../../styles';
+import { useNavbarStyles, WhiteTooltip } from '../../styles';
 import logo from '../../images/logo.png';
-import { defaultCurrentUser } from '../../data';
+import { defaultCurrentUser, getDefaultUser } from '../../data';
 
 function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
@@ -27,7 +27,7 @@ function Navbar({ minimalNavbar }) {
         <Logo />
         {!minimalNavbar && (
           <>
-            <Search />
+            <Search history={history} />
             <Links path={path} />
           </>
         )}
@@ -50,11 +50,19 @@ function Logo() {
   );
 }
 
-function Search() {
+function Search({ history }) {
   const classes = useNavbarStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [results, setResults] = React.useState([]);
   const [query, setQuery] = React.useState('');
 
-  let loading = false;
+  const hasResults = Boolean(query) && results.length > 0;
+
+  React.useEffect(() => {
+    if (!query.trim()) return;
+
+    setResults(Array.from({ length: 5 }, () => getDefaultUser()));
+  }, [query]);
 
   function handleClearInput() {
     setQuery('');
@@ -62,20 +70,58 @@ function Search() {
 
   return (
     <Hidden xsDown>
-      <InputBase
-        placeholder='search'
-        className={classes.input}
-        onChange={(event) => setQuery(event.target.value)}
-        startAdornment={<span  className={classes.searchIcon} />}
-        endAdornment={
-          loading ? (
-            <LoadingIcon />
-          ) : (
-            <span className={classes.clearIcon} onClick={handleClearInput} />
+      <WhiteTooltip
+        arrow
+        interactive
+        TransitionComponent={Fade}
+        open={hasResults}
+        title={
+          hasResults && (
+            <Grid className={classes.resultContainer} container>
+              {results.map((result) => (
+                <Grid
+                  key={result.id}
+                  className={classes.resultLink}
+                  onClick={() => {
+                    history.push(`/${result.username}`);
+                    handleClearInput();
+                  }}
+                  item
+                >
+                  <div className={classes.resultWrapper}>
+                    <div className={classes.avatarWrapper}>
+                      <Avatar src={result.profile_image} alt='User Avatar' />
+                    </div>
+                    <div className={classes.nameWrapper}>
+                      <Typography variant='body1'>
+                        {result.username}
+                      </Typography>
+                      <Typography variant='body2' color='textSecondary'>
+                        {result.name}
+                      </Typography>
+                    </div>
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
           )
         }
-        value={query}
-      />
+      >
+        <InputBase
+          placeholder='search'
+          className={classes.input}
+          onChange={(event) => setQuery(event.target.value)}
+          startAdornment={<span  className={classes.searchIcon} />}
+          endAdornment={
+            loading ? (
+              <LoadingIcon />
+            ) : (
+              <span className={classes.clearIcon} onClick={handleClearInput} />
+            )
+          }
+          value={query}
+        />
+      </WhiteTooltip>
     </Hidden>
   );
 }
