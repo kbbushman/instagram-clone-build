71a6342b87acabdb32f34ed2f56c3e51e4ae2938
kbbushman <k.bushman@yahoo.com>
2021-03-14 22:42:01 -0700

    Adds user card

--- src/components/shared/UserCard.js
+++ src/components/shared/UserCard.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { useUserCardStyles } from "../../styles";
+import { Link } from 'react-router-dom';
+import { Avatar, Typography } from '@material-ui/core';
+import { useUserCardStyles } from '../../styles';
+import { defaultUser } from '../../data';
 
-function UserCard() {
-  useUserCardStyles();
+function UserCard({ user = defaultUser, avatarSize = 44 }) {
+  const classes = useUserCardStyles({ avatarSize });
+  const { username, name, profile_image } = user;
 
-  return <div>UserCard</div>;
+  return (
+    <div className={classes.wrapper}>
+      <Link to={`/${username}`}>
+        <Avatar
+          alt='User Avatar'
+          src={profile_image}
+          className={classes.avatar}
+        />
+      </Link>
+      <div className={classes.nameWrapper}>
+        <Link to={`/${username}`}>
+          <Typography variant='subtitle2' className={classes.typography}>
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color='textSecondary'
+          variant='body2'
+          className={classes.typography}
+        >
+          {name}
+        </Typography>
+      </div>
+    </div>
+  );
 }
 
 export default UserCard;
