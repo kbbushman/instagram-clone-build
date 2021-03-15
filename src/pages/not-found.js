284d3fadf5377efda3ee7cea5465f5537d211f6f
kbbushman <k.bushman@yahoo.com>
2021-03-15 14:07:03 -0700

    Adds minimalNavbar prop

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -5,7 +5,7 @@ import Layout from '../components/shared/Layout';
 
 function NotFoundPage() {
   return (
-    <Layout title='Page Not Found' marginTop={120}>
+    <Layout title='Page Not Found' marginTop={120} minimalNavbar>
       <Typography variant='h5' align='center' paragraph>
         Sorry, this page isn't available.
       </Typography>
