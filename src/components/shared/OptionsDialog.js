b711b2ca5b01478a69c770eae4fd0b6b115d55fe
kbbushman <k.bushman@yahoo.com>
2021-03-18 16:47:39 -0700

    Adds options dialog menu

--- src/components/shared/OptionsDialog.js
+++ src/components/shared/OptionsDialog.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { useOptionsDialogStyles } from "../../styles";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Dialog, Divider, Zoom } from '@material-ui/core';
+import { useOptionsDialogStyles } from '../../styles';
+import { defaultPost } from '../../data';
 
-function OptionsDialog() {
-  useOptionsDialogStyles();
+function OptionsDialog({ onClose }) {
+  const classes = useOptionsDialogStyles();
 
-  return <div>OptionsDialog</div>;
+  return (
+    <Dialog
+      classes={{
+        scrollPaper: classes.dialogScrollPaper
+      }}
+      onClose={onClose}
+      TransitionComponent={Zoom}
+      open
+    >
+      <Button className={classes.redButton}>
+        Unfollow
+      </Button>
+      <Divider />
+      <Button className={classes.button}>
+        <Link to={`/p/${defaultPost.id}`}>
+          Go to post
+        </Link>
+      </Button>
+      <Divider />
+      <Button className={classes.button}>
+        Share
+      </Button>
+      <Divider />
+      <Button className={classes.button}>
+        Copy Link
+      </Button>
+      <Button className={classes.button} onClick={onClose}>
+        Cancel
+      </Button>
+    </Dialog>
+  );
 }
 
 export default OptionsDialog;
