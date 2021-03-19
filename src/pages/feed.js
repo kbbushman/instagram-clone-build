acfb87692f0b7f56cc12404592ad35ee84846af0
kbbushman <k.bushman@yahoo.com>
2021-03-19 14:04:29 -0700

    Adds react lazy/suspense for FeedPost with skeleton fallback

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Hidden } from '@material-ui/core';
 import Layout from '../components/shared/Layout';
 import UserCard from '../components/shared/UserCard';
-import FeedPost from '../components/feed/FeedPost';
+// import FeedPost from '../components/feed/FeedPost';
 import FeedSideSuggestions from '../components/feed/FeedSideSuggestions';
 import LoadingScreen from '../components/shared/LoadingScreen';
 import { LoadingLargeIcon } from '../icons';
 import { useFeedPageStyles } from '../styles';
 import { getDefaultPost } from '../data';
+import FeedPostSkeleton from '../components/feed/FeedPostSkeleton';
+const FeedPost = React.lazy(() => import('../components/feed/FeedPost'));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
@@ -22,7 +24,9 @@ function FeedPage() {
         {/* FEED POSTS */}
         <div>
           {Array.from({ length: 5 }, () => getDefaultPost()).map((post, index) => (
-            <FeedPost key={post.id} index={index} post={post} />
+            <React.Suspense  key={post.id} fallback={<FeedPostSkeleton />}>
+              <FeedPost index={index} post={post} />
+            </React.Suspense>
           ))}
         </div>
         {/* SIDEBAR */}
