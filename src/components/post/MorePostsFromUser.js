b02828650a31713e0ae3dadb2aa0a6e6ea633fff
kbbushman <k.bushman@yahoo.com>
2021-03-18 23:22:29 -0700

    Adds more posts from user

--- src/components/post/MorePostsFromUser.js
+++ src/components/post/MorePostsFromUser.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { useMorePostsFromUserStyles } from "../../styles";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
+import GridPost from '../shared/GridPost';
+import { useMorePostsFromUserStyles } from '../../styles';
+import { LoadingLargeIcon } from '../../icons';
+import { getDefaultPost, defaultUser } from '../../data';
 
 function MorePostsFromUser() {
-  useMorePostsFromUserStyles();
+  const classes = useMorePostsFromUserStyles();
 
-  return <div>MorePostsFromUser</div>;
+  let loading = false;
+
+  return (
+    <div className={classes.container}>
+      <Typography
+        color='textSecondary'
+        variant='subtitle2'
+        component='h2'
+        className={classes.typography}
+        gutterBottom
+      >
+        More Posts From{' '}
+        <Link to={`/${defaultUser.username}`} className={classes.link}>
+          @{defaultUser.username}
+        </Link>
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 6 }, () => getDefaultPost()).map((post) => (
+              <GridPost key={post.id} post={post} />
+            ))}
+          </div>
+        </article>
+      )}
+    </div>
+  );
 }
 
 export default MorePostsFromUser;
