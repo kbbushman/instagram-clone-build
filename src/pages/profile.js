00df9d99865ff3cf6521d7f4aea8be0f07570aab
kbbushman <k.bushman@yahoo.com>
2021-03-19 14:35:07 -0700

    Adds base profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Card, CardContent, Hidden } from '@material-ui/core';
 import Layout from '../components/shared/Layout';
 import { useProfilePageStyles } from '../styles';
+import { defaultCurrentUser } from '../data';
+import ProfilePicture from '../components/shared/ProfilePicture';
 
 function ProfilePage() {
-  useProfilePageStyles();
+  const classes = useProfilePageStyles();
 
-  return <Layout>ProfilePage</Layout>;
+  return (
+    <Layout title={`${defaultCurrentUser.name} @${defaultCurrentUser.username}`}>
+      <div className={classes.container}>
+        <Hidden xsDown>
+          <Card className={classes.cardLarge}>
+            <ProfilePicture />
+            <CardContent className={classes.cardContentLarge}>
+              <ProfileNameSection />
+              <PostCountSection />
+              <NameBioSection />
+            </CardContent>
+          </Card>
+        </Hidden>
+        <Hidden smUp>
+          <Card className={classes.cardSmall}>
+            <CardContent>
+              <section className={classes.sectionSmall}>
+                <ProfilePicture />
+                <ProfileNameSection />
+              </section>
+              <NameBioSection />
+            </CardContent>
+            <PostCountSection />
+          </Card>
+        </Hidden>
+      </div>
+    </Layout>
+  );
+}
+
+function ProfileNameSection() {
+  return (
+    <>
+      ProfileNameSection
+    </>
+  );
+}
+
+function PostCountSection() {
+  return (
+    <>
+      PostCountSection
+    </>
+  );
+}
+
+function NameBioSection() {
+  return (
+    <>
+      ProfileNameSection
+    </>
+  );
 }
 
 export default ProfilePage;
