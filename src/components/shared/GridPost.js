c56eb47d1a23b6980299926bf70dd95b8caab152
kbbushman <k.bushman@yahoo.com>
2021-03-18 22:06:04 -0700

    Adds grid post

--- src/components/shared/GridPost.js
+++ src/components/shared/GridPost.js
@@ -1,10 +1,34 @@
-import React from "react";
-import { useGridPostStyles } from "../../styles";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
+import { useGridPostStyles } from '../../styles';
 
-function GridPost() {
-  useGridPostStyles();
+function GridPost({ post }) {
+  const history = useHistory();
+  const classes = useGridPostStyles();
 
-  return <div>GridPost</div>;
+  function handleOpenPostModal() {
+    history.push({
+      pathname: `/p/${post.id}`,
+      state: { modal: true }
+    });
+  }
+
+  return (
+    <div onClick={handleOpenPostModal} className={classes.gridPostContainer}>
+      <div className={classes.gridPostOverlay}>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.likes} />
+          <Typography>{post.likes}</Typography>
+        </div>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.comments} />
+          <Typography>{post.comments.length}</Typography>
+        </div>
+      </div>
+      <img src={post.media} className={classes.image} alt='Post Cover' />
+    </div>
+  );
 }
 
 export default GridPost;
