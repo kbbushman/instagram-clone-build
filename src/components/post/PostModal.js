b7ab71481363b8054dd29807bcd9e9abfdfd5928
kbbushman <k.bushman@yahoo.com>
2021-03-18 22:33:39 -0700

    Adds post modal

--- src/components/post/PostModal.js
+++ src/components/post/PostModal.js
@@ -1,10 +1,46 @@
-import React from "react";
-import { usePostModalStyles } from "../../styles";
+import Modal from 'react-modal';
+import { useHistory, useParams } from 'react-router';
+import Post from './Post';
+import { CloseIcon } from '../../icons';
+import { usePostModalStyles } from '../../styles';
 
 function PostModal() {
-  usePostModalStyles();
+  const history = useHistory();
+  const { postId } = useParams();
+  const classes = usePostModalStyles();
 
-  return <div>PostModal</div>;
+  return (
+    <>
+      <Modal
+        overlayClassName={classes.overlay}
+        onRequestClose={() => history.goBack()}
+        ariaHideApp={false}
+        style={{
+          content: {
+            display: 'flex',
+            alignItems: 'center',
+            maxWidth: 935,
+            width: '100%',
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+            margin: 0,
+            padding: 0,
+            overflow: 'none',
+            WebkitOverflowScrolling: 'touch',
+          }
+        }}
+        isOpen
+      >
+        <Post id={postId} />
+      </Modal>
+      <div className={classes.close} onClick={() => history.goBack()}>
+        <CloseIcon />
+      </div>
+    </>
+  );
 }
 
 export default PostModal;
