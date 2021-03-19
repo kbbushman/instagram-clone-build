b012292d4c72881f72fbe9be8a16f393cc55af4e
kbbushman <k.bushman@yahoo.com>
2021-03-18 22:05:10 -0700

    Adds router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import theme from "./theme";
 import App from "./App";
@@ -7,7 +8,9 @@ import App from "./App";
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </MuiThemeProvider>,
   document.getElementById("root")
 );
