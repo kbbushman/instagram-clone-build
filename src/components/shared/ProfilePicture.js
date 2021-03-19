1cfbe071da4e2f2d5dc94c972eaf1fee2360b574
kbbushman <k.bushman@yahoo.com>
2021-03-19 15:43:29 -0700

    Adds user profile image

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,27 @@
-import React from "react";
-import { useProfilePictureStyles } from "../../styles";
+import React from 'react';
+import { Person } from '@material-ui/icons';
+import { useProfilePictureStyles } from '../../styles';
 
-function ProfilePicture() {
-  useProfilePictureStyles();
+function ProfilePicture({
+  image = 'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg',
+  size,
+  isOwner
+}) {
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt='User Profile' className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
