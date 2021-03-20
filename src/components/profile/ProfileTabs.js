fd4941f0979f63cee350cb72c4deca82569cc233
kbbushman <k.bushman@yahoo.com>
2021-03-19 21:14:49 -0700

    Adds profile tabs with profile posts and user saved posts

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -1,10 +1,119 @@
-import React from "react";
-import { useProfileTabsStyles } from "../../styles";
+import React from 'react';
+import { Divider, Hidden, Tab, Tabs, Typography } from '@material-ui/core';
+import GridPost from '../shared/GridPost';
+import { useProfileTabsStyles } from '../../styles';
+import { GridIcon, SaveIcon } from '../../icons';
 
-function ProfileTabs() {
-  useProfileTabsStyles();
+function ProfileTabs({ user, isOwner }) {
+  const classes = useProfileTabsStyles();
+  const [value, setValue] = React.useState(0);
 
-  return <div>ProfileTabs</div>;
+  return (
+    <>
+      <section className={classes.section}>
+        <Hidden xsDown>
+          <Divider />
+        </Hidden>
+        <Hidden xsDown>
+          <Tabs
+            value={value}
+            onChange={(_, value) => setValue(value)}
+            classes={{ indicator: classes.tabsIndicator }}
+            centered
+          >
+            <Tab
+              label='POSTS'
+              classes={{
+                root: classes.tabRoot,
+                labelIcon: classes.tabLabelIcon,
+                wrapper: classes.tabWrapper,
+              }}
+              icon={<span className={classes.postsIconLarge} />}
+            />
+            {isOwner && (
+              <Tab
+                label='SAVED'
+                classes={{
+                  root: classes.tabRoot,
+                  labelIcon: classes.tabLabelIcon,
+                  wrapper: classes.tabWrapper,
+                }}
+                icon={<span className={classes.savedIconLarge} />}
+              />
+            )}
+          </Tabs>
+        </Hidden>
+        <Hidden smUp>
+          <Tabs
+            value={value}
+            onChange={(_, value) => setValue(value)}
+            className={classes.tabs}
+            classes={{ indicator: classes.tabsIndicator }}
+            centered
+          >
+            <Tab
+              icon={<GridIcon fill={value === 0 ? '#3897f0' : undefined} />}
+              classes={{ root: classes.tabRoot }}
+            />
+            {isOwner && (
+              <Tab
+                icon={<SaveIcon fill={value === 1 ? '#3897f0' : undefined} />}
+                classes={{ root: classes.tabRoot }}
+              />
+            )}
+          </Tabs>
+        </Hidden>
+        <Hidden smUp>{user.posts.length === 0  && <Divider />}</Hidden>
+        {value === 0 && <ProfilePosts user={user} isOwner={isOwner} />}
+        {value === 1 && <SavedPosts />}
+      </section>
+    </>
+  );
+}
+
+function ProfilePosts({ user, isOwner }) {
+  const classes = useProfileTabsStyles();
+
+  if (user.posts.length === 0) {
+    return (
+      <section className={classes.profilePostsSection}>
+        <div className={classes.noContent}>
+          <div className={classes.uploadPhotoIcon} />
+          <Typography variant='h4'>
+            {isOwner ? 'Upload a Photo' : 'No Photos'}
+          </Typography>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <article className={classes.article}>
+      <div className={classes.postContainer}>
+        {user.posts.map((post) => (
+          <GridPost key={post.id} post={post} />
+        ))}
+      </div>
+    </article>
+  );
+}
+
+function SavedPosts() {
+  const classes = useProfileTabsStyles();
+
+  return (
+    <section className={classes.savedPostsSection}>
+      <div className={classes.noContent}>
+        <div className={classes.savePhotoIcon} />
+        <Typography variant='h4'>
+          Save
+        </Typography>
+        <Typography align='center'>
+          Save photos and videos that you want to see again. No one is notified, and only you can see what you've saved.
+        </Typography>
+      </div>
+    </section>
+  );
 }
 
 export default ProfileTabs;
