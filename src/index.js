c19289f4f9100c6a82c227dcea3e77fa77b76ede
kbbushman <k.bushman@yahoo.com>
2021-03-21 17:57:19 -0700

    Adds AuthProvider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import theme from "./theme";
 import App from "./App";
+import AuthProvider from './auth';
+import client from './graphql/client';
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <Router>
-      <App />
-    </Router>
-  </MuiThemeProvider>,
+  <AuthProvider>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <App />
+        </Router>
+      </MuiThemeProvider>
+    </ApolloProvider>
+  </AuthProvider>,
   document.getElementById("root")
 );
